895441e00fee485c81124f1d7c865c01a3e592d4
PinozenTH <pinozen.thailand@gmail.com>
2025-10-03 12:45:28 +0700

    implement banking system main menu and login functionality

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -19,7 +21,147 @@ public class Main {
     };
 
     public static void main(String[] args) {
+        System.out.println("Welcome to the Banking System");
+        System.out.print("""
+                Please select an option:
+                1. Create Account
+                2. Login
+                3. Exit
+                Your choice:\s"""); // \s is adding 1 whitespace " "
+        Scanner scanner = new Scanner(System.in);
+        boolean landing = true;
+        while (landing) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1: {
+                    landing = false;
+                    break;
+                }
+                case 2: {
+                    landing = false;
+                    login();
+                    break;
+                }
+                case 3: {
+                    landing = false;
+                    System.out.println("Exiting...\n" +
+                            "Thank you for using the Banking System");
+                    System.exit(0);
+                    break;
+                }
+                default: {
+                    System.out.print("""
+                            ===================================
+                             Invalid choice, Please try again.
+                            ===================================
+                            Please select an option:
+                            1. Create Account
+                            2. Login
+                            3. Exit
+                            Your choice:\s""");
+                    break;
+                }
+            }
+        }
+    }
 
+    private static void login() {
+        Scanner scanner = new Scanner(System.in);
+        String account = null;
+        int accountId = -1;
+        boolean loggingIn = true;
+        while (loggingIn) {
+            account = getAccount(scanner); // get account number from user
+
+            if (isAccountSyntaxErr(account)) { // validate account: String is "", Account number length > 5
+                continue; // continue the loop
+            }
+
+            accountId = getAccountId(account); // get account index from accounts array
+            if (accountId == -1) { // validate accountId
+                System.out.println("Please try again..."); // print error message
+                continue; // continue the loop
+            }
+
+            loggingIn = false; // exit the loop
+        }
+
+        System.out.println("Login successful\n" +
+                "Welcome, " + account);
+
+        boolean loggedIn = true;
+        while (loggedIn) {
+            System.out.print("""
+                    Please select an option:
+                    1. View Balance
+                    2. Deposit
+                    3. Withdraw
+                    4. Transfer
+                    5. Logout
+                    Your choice:\s""");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1: {
+                    System.out.println("Your balance is: " + getBalance(accountId));
+                    break;
+                }
+                case 2: {
+                    System.out.print("Enter amount to deposit: ");
+                    int amount = scanner.nextInt();
+                    scanner.nextLine();
+//                    deposit(accountId, amount);
+                    break;
+                }
+                case 3: {
+                    System.out.print("Enter amount to withdraw: ");
+                    int amount = scanner.nextInt();
+                    scanner.nextLine();
+//                    withdraw(accountId, amount);
+                    break;
+                }
+                case 4: {
+                    System.out.print("Enter recipient account number: ");
+                    String toAccount = scanner.nextLine();
+                    int toAccountId = getAccountId(toAccount);
+                    if (accountId == toAccountId) {
+                        System.out.println("Cannot transfer to the same account");
+                        break;
+                    }
+                    if (toAccountId == -1) {
+                        System.out.println("Invalid recipient account");
+                        break;
+                    }
+                    System.out.print("Enter amount to transfer: ");
+                    int amount = scanner.nextInt();
+                    if (balances[accountId] < amount) {
+                        System.out.println("Insufficient balance");
+                        break;
+                    }
+                    scanner.nextLine();
+//                    transfer(accountId, toAccountId, amount);
+                    break;
+                }
+                case 5: {
+                    loggedIn = false;
+//                    logout();
+                    break;
+                }
+                default: {
+                    System.out.println("""
+                    ===================================
+                     Invalid choice, Please try again.
+                    ===================================""");
+                    break;
+                }
+            }
+        }
+    }
+
+    private static String getAccount(Scanner scanner) {
+        System.out.print("Enter account number (Must be 5 digit long): ");
+        return scanner.nextLine();
     }
 
     private static int getAccountId(String account) { // get account index from accounts array
